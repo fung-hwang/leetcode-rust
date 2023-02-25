c88582f2451637bf4ca3c17eb6affc9ef18c3c80
zhoushaofeng <zhoushaofeng@mail.ustc.edu.cn>
2023-02-26 05:33:12 +0800

    Framework init

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// 类型1
+mod s0001;
+
+pub struct Solution;
+
+fn main() {
+    println!("leetcode rust");
+}
---
+++ src/s0001.rs
@@ -0,0 +1,15 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_0001() {}
+}
